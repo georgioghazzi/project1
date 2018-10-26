dd67f2850d0324a18a6af0de25821c28dbabc16d
Georgio Ghazzi <ghazzi94@gmail.com>
2018-10-27 00:06:23 +0300

    Fixed Admin Page

--- admin/routes/web.php
+++ admin/routes/web.php
@@ -10,15 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/','HomeController@index', function () {
-    return view('front/home');
-})->name('home');
-Route::get('/add-to-cart/{id}','HomeController@getAddtoCart')->name('add.to.cart');
-Route::get('/shopping-cart','HomeController@getCart')->name('shoping.cart');
-Route::get('/checkout','HomeController@checkout')->name('checkout')->middleware('auth');
-Route::get('/guest_checkout','HomeController@guestCheckout')->name('guestCheckout');
-Route::post('/checkout','HomeController@postCheckout')->name('postCheckout');
+Route::get('/', 'AdminController@index')->name('admin.dashboard');
 
 Auth::routes();
 
@@ -26,7 +18,7 @@ Route::get('/home', 'HomeController@index')->name('front.home');
 Route::prefix('admin')->group(function(){
     Route::get('/login','Auth\AdminLoginController@showLoginForm')->name('admin.login');
     Route::post('/login','Auth\AdminLoginController@login')->name('admin.login.submit');
-    Route::get('/', 'AdminController@index')->name('admin.dashboard');
+
     Route::get('/recipes','AdminController@Recipes')->name('admin.recipes')->middleware('role:chef,admin');
     Route::get('/orders','AdminController@orders')->name('admin.orders')->middleware('role:staff,admin');
     Route::get('/users','AdminController@users')->name('admin.users')->middleware('role:admin');
